6845ed0c3c7433aeb66a0811eba6ec905b89fd84
alex-from-ubuntu <chalghoos1@gmail.com>
2020-08-07 19:35:09 +0430

    session 5 adding welcom and root directory

--- app.py
+++ app.py
@@ -7,3 +7,20 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "hello this is index page"
+
+# @app.route("/welcome/<name>")
+# def welcome(name):
+#     return "welcome %s" % (name)
+
+# @app.route("/welcome/<string:name>")  # string can be channge with: int float path uuid
+# def welcome_name(name):
+#     return "welcome %s" % (name)
+
+@app.route('/welcome/')
+@app.route('/welcome/<int:id>')
+def welcome(id = None): # for set default none
+    massage = "The id is " + str(id)
+    if id is None:
+        massage = "id should not be none"
+    return massage
+
